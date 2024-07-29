296c76ec16425c9816f1ec3352d126a50e450684
Firemanhyeon <ghgus990129@naver.com>
2024-07-29 22:25:30 +0900

    07.29

--- src/main/java/org/blog/blogflatformproject/config/SecurityConfig.java
+++ src/main/java/org/blog/blogflatformproject/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.blog.blogflatformproject.config;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.blog.blogflatformproject.jwt.exception.CustomAuthenticationEntryPoint;
 import org.blog.blogflatformproject.jwt.filter.JwtAuthenticationFilter;
@@ -13,8 +14,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -24,8 +23,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,22 +34,29 @@ public class SecurityConfig {
     private final CustomOAuth2AthenticationSuccessHandler customOAuth2AthenticationSuccessHandler;
 
 
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenizer jwtTokenizer, CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenizer jwtTokenizer,
+                                                   CustomAuthenticationEntryPoint customAuthenticationEntryPoint)
+            throws Exception {
         http
                 .authorizeRequests(authorize -> authorize
-                        .requestMatchers("/", "/user/loginform", "/user/signInForm", "/board/boardInfo/**", "/blog/**", "/api/user/duplicateChk",
-                                "/user/userreg", "/user/login","/login", "/user/welcome", "/css/**", "/js/**","/Users/jeonghohyeon/Desktop/blogUserImage/**"
-                                ,"/api/user/login","/api/user/getUser","/api/user/updatename","/user/logout","/board/boardInfo/**"
-                                ,"/board/getReplies","/api/user/getFollowCnt","/api/user/getFollowingCnt","/board/getSeriesInfo/**"
-                                ,"/board/selectVal","board/mySelectVal","/image/**","/login/oauth2/code/github","/user/registerSocialUser/**"
-                        ,"/user/saveSocialUser","/oauth2/**","/api/user/auth/kakao/callback","/board/searchVal").permitAll()
+                        .requestMatchers("/", "/user/loginform", "/user/signInForm", "/board/boardInfo/**", "/blog/**",
+                                "/api/user/duplicateChk",
+                                "/user/userreg", "/user/login", "/login", "/user/welcome", "/css/**", "/js/**",
+                                "/Users/jeonghohyeon/Desktop/blogUserImage/**"
+                                , "/api/user/login", "/api/user/getUser", "/api/user/updatename", "/user/logout",
+                                "/board/boardInfo/**"
+                                , "/board/getReplies", "/api/user/getFollowCnt", "/api/user/getFollowingCnt",
+                                "/board/getSeriesInfo/**"
+                                , "/board/selectVal", "board/mySelectVal", "/image/**", "/login/oauth2/code/github",
+                                "/user/registerSocialUser/**"
+                                , "/user/saveSocialUser", "/oauth2/**", "/api/user/auth/kakao/callback",
+                                "/board/searchVal").permitAll()
                         .anyRequest()
                         .authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenizer), UsernamePasswordAuthenticationFilter.class)
-                .formLogin(form-> form.disable())
+                .formLogin(form -> form.disable())
 //                .logout(logout -> logout
 //                        .logoutUrl("/user/logout")
 //                        .logoutSuccessUrl("/")
@@ -61,9 +65,9 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .csrf(csrf -> csrf.disable())
-                .httpBasic(httpBasic->httpBasic.disable())
-                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
-                .exceptionHandling(exception->exception
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(customAuthenticationEntryPoint))
                 .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
                 .oauth2Login(oauth2 -> oauth2
@@ -76,21 +80,18 @@ public class SecurityConfig {
         return http.build();
     }
 
-    public CorsConfigurationSource corsConfigurationSource(){
+    public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
-        config.setAllowedMethods(List.of("GET" , "POST" , "DELETE" , "PATCH" , "OPTION" , "PUT"));
+        config.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH", "OPTION", "PUT"));
         config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**",config);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 
-
-
-    //비밀번호 암호화해서 넣기
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
         DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
@@ -100,7 +101,6 @@ public class SecurityConfig {
             // 예: DB에 사용자 정보 저장, 권한 부여 등
 
             String token = oauth2UserRequest.getAccessToken().getTokenValue();
-
 
             // Save or update the user in the database
             String provider = oauth2UserRequest.getClientRegistration().getRegistrationId();
